a3582d1938325b108e19fc9418867ab830e18cc1
Harshad015 <harshadghormade@gmail.com>
2022-01-30 21:22:39 +0530

    Pass1

--- Practice/force-app/main/default/lwc/child/child.js
+++ Practice/force-app/main/default/lwc/child/child.js
@@ -1,20 +1,5 @@
-import { LightningElement } from 'lwc';
+import { api, LightningElement } from 'lwc';
 
 export default class Child extends LightningElement {
-    constructor(){
-        super()
-        console.log("child constructor is loaded");
-    }
-
-    renderedCallback(){
-        console.log("child Render callBack is renderd");
-    }
-    
-    connectedCallback(){
-        console.log("child Connected callback is rendered UPsated");
-        throw new Error("loading of child component faild");
-    }
-    disconnectedCallback(){
-        alert("Disconnected callback is call!!")
-    }
+    @api SupPower;
 }
